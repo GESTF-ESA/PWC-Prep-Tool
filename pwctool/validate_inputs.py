0280dbd5e4ec9af49a1cbdef8bc5695b3928fce3
loganinsingaAAS <74274071+loganinsingaAAS@users.noreply.github.com>
2023-08-24 12:12:32 -0700

    simplify error messaging in validate_inputs.py

--- pwctool/validate_inputs.py
+++ pwctool/validate_inputs.py
@@ -8,6 +8,12 @@ import pandas as pd
 from PyQt5.QtWidgets import QDialog
 
 
+def _display_error_message(error_dialog: QDialog, message: str):
+    error_dialog.errMsgLabel.setText(message)
+    error_dialog.exec_()
+    return False
+
+
 def validate_input_files(config: dict[str, Any], error_dialog: QDialog):
     """Validates the input files prior to tool execution. Notifies user
     if something is incorrect and prevents execution."""
@@ -26,62 +32,38 @@ def _validate_config(config: dict[str, Any], error_dialog: QDialog) -> bool:
     """Checks that each input file/folder in the config exists"""
 
     if not os.path.exists(config["FILE_PATHS"]["OUTPUT_DIR"]):
-        error_dialog.errMsgLabel.setText(
-            "Output directory does not exist. Please choose a valid directory and try again."
-        )
-        error_dialog.exec_()
-        return False
+        err_message = "Output directory does not exist. Please choose a valid directory and try again."
+        _display_error_message(error_dialog, err_message)
 
     if not os.path.exists(config["FILE_PATHS"]["AGRONOMIC_PRACTICES_EXCEL"]):
-        error_dialog.errMsgLabel.setText(
-            "The agronomic practices table does not exist or the path is incorrect. Please ensure it is valid and try again."
-        )
-        error_dialog.exec_()
-        return False
+        err_message = "The agronomic practices table does not exist or the path is incorrect. Please ensure it is valid and try again."
+        _display_error_message(error_dialog, err_message)
 
     if not os.path.exists(config["FILE_PATHS"]["AGDRIFT_REDUCTION_TABLE"]):
-        error_dialog.errMsgLabel.setText(
-            "The AgDRIFT Reduction table does not exist or the path is incorrect. Please ensure it is valid and try again."
-        )
-        error_dialog.exec_()
-        return False
+        err_message = "The AgDRIFT Reduction table does not exist or the path is incorrect. Please ensure it is valid and try again."
+        _display_error_message(error_dialog, err_message)
 
     if not os.path.exists(config["FILE_PATHS"]["SCENARIO_FILES_PATH"]):
-        error_dialog.errMsgLabel.setText(
-            "The scenario files directory does not exist or the path is incorrect. Please ensure it is valid and try again."
-        )
-        error_dialog.exec_()
-        return False
+        err_message = "The scenario files directory does not exist or the path is incorrect. Please ensure it is valid and try again."
+        _display_error_message(error_dialog, err_message)
 
     if config["USE_CASE"] == "Use Case #1":
         if not os.path.exists(config["FILE_PATHS"]["INGR_FATE_PARAMS"]):
-            error_dialog.errMsgLabel.setText(
-                "The ingr. fate parameters table does not exist or the path is incorrect. Please ensure it is valid and try again."
-            )
-            error_dialog.exec_()
-            return False
+            err_message = "The ingr. fate parameters table does not exist or the path is incorrect. Please ensure it is valid and try again."
+            _display_error_message(error_dialog, err_message)
 
         if not os.path.exists(config["FILE_PATHS"]["WETTEST_MONTH_CSV"]):
-            error_dialog.errMsgLabel.setText(
-                "The wettest month table does not exist or the path is incorrect. Please ensure it is valid and try again."
-            )
-            error_dialog.exec_()
-            return False
+            err_message = "The wettest month table does not exist or the path is incorrect. Please ensure it is valid and try again."
+            _display_error_message(error_dialog, err_message)
 
         if not os.path.exists(config["FILE_PATHS"]["BIN_TO_LANDSCAPE"]):
-            error_dialog.errMsgLabel.setText(
-                "The bin to landscape lookup table does not exist or the path is incorrect. Please ensure it is valid and try again."
-            )
-            error_dialog.exec_()
-            return False
+            err_message = "The bin to landscape lookup table does not exist or the path is incorrect. Please ensure it is valid and try again."
+            _display_error_message(error_dialog, err_message)
 
     elif config["USE_CASE"] == "Use Case #2":
         if not os.path.exists(config["FILE_PATHS"]["PWC_BATCH_CSV"]):
-            error_dialog.errMsgLabel.setText(
-                "The input pwc batch file does not exist or the path is incorrect. Please ensure it is valid and try again."
-            )
-            error_dialog.exec_()
-            return False
+            err_message = "The input pwc batch file does not exist or the path is incorrect. Please ensure it is valid and try again."
+            _display_error_message(error_dialog, err_message)
 
     if config["RUN_ID"] == "":
         error_dialog.errMsgLabel.setText("Please create a run id and try again.")
@@ -95,6 +77,7 @@ def _validate_apt(config: dict[str, Any], error_dialog: QDialog):
     """Validates the ag practices table. Checks:
         - the first column of the APT is "RunDescriptor"
         - check that the annual restrictions are entered
+        - check that the types are correct
         - check that a value is entered for either pre or post E MRI for all rates
         - rate 1 has valid max app rate specified
         - if MRI is specified but lacks other rate info, notify user
@@ -110,18 +93,37 @@ def _validate_apt(config: dict[str, Any], error_dialog: QDialog):
     try:
         ag_practices.set_index(keys="RunDescriptor", inplace=True)
     except KeyError:
-        error_dialog.errMsgLabel.setText("Please ensure the first column of the APT is 'RunDescriptor' and try again.")
-        error_dialog.exec_()
-        return False
+        err_message = "Please ensure the first column of the APT is 'RunDescriptor' and try again."
+        _display_error_message(error_dialog, err_message)
 
     for indx, row in ag_practices.iterrows():
         # check that the annual restrictions and PHI are entered
         if any(pd.isna(y) for y in [row["MaxAnnAmt"], row["MaxAnnNumApps"], row["PHI"]]):
-            error_dialog.errMsgLabel.setText(
-                f"It looks like the annual restrictions or PHI are not specified for {indx} in Ag Practices Table. Please ensure these are entered correctly and try again."
+            err_message = f"It looks like the annual restrictions or PHI are not specified for {indx} in Ag Practices Table. Please ensure these are entered correctly and try again."
+            _display_error_message(error_dialog, err_message)
+
+        # check that the annual maximum number of apps and PHI are integers
+        if not all(isinstance(b, int) for b in [row["MaxAnnNumApps"], row["PHI"]]):
+            err_message = f"Please ensure annual MaxAnnNumApps and PHI for {indx} in Ag Practices Table are integers and try again."
+            _display_error_message(error_dialog, err_message)
+
+        # check that the annual maximum amount is an integer or float
+        if not isinstance(row["MaxAnnAmt"], (int, float)):
+            err_message = (
+                f"Please ensure the AnnMaxAmt for {indx} in Ag Practices Table is an integer or float and try again."
             )
-            error_dialog.exec_()
-            return False
+            _display_error_message(error_dialog, err_message)
+
+        # check that the pre E and post E maxamt and maxnumapps are the correct type if entered
+        for max_amt in ["PreEmergence_MaxAmt", "PostEmergence_MaxAmt"]:
+            if pd.notna(row[max_amt]):
+                err_message = f"Please ensure the PreEmergence_MaxAmt and PostEmergence_MaxAmt for {indx} in Ag Practices Table are an integer or float and try again."
+                _display_error_message(error_dialog, err_message)
+
+        for max_amt in ["PreEmergence_MaxNumApps", "PostEmergence_MaxNumApps"]:
+            if pd.notna(row[max_amt]):
+                err_message = f"Please ensure the PreEmergence_MaxNumApps and PostEmergence_MaxNumApps for {indx} in Ag Practices Table are integers and try again."
+                _display_error_message(error_dialog, err_message)
 
         # check rate info validity
         for i in [1, 2, 3, 4]:
@@ -134,29 +136,20 @@ def _validate_apt(config: dict[str, Any], error_dialog: QDialog):
             # check if either pre or post E MRI is specified for any rates that have info entered
             if any(pd.notna(j) for j in [max_app_rate, max_num_apps, rate_instr]):  # at least one is not nan
                 if all(pd.isna(x) for x in [pre_e_mri, post_e_mri]):
-                    error_dialog.errMsgLabel.setText(
-                        f"Either the Pre Emergence or Post Emergence MRI is not be specified for rate {i} for {indx} in Ag Practices Table. Either the Pre E or Post E MRI must be specified for any valid rate. Please see the reader's guide for more information."
-                    )
-                    error_dialog.exec_()
-                    return False
+                    err_message = f"Either the Pre Emergence or Post Emergence MRI is not be specified for rate {i} for {indx} in Ag Practices Table. Either the Pre E or Post E MRI must be specified for any valid rate. Please see the reader's guide for more information."
+                    _display_error_message(error_dialog, err_message)
 
             # make sure rate 1 has max app rate specified
             if i == 1:
                 if pd.isna(max_app_rate):
-                    error_dialog.errMsgLabel.setText(
-                        f"It looks like there is no max app rate specified for Rate 1 for {indx} in the Ag Practices Table. Rate 1 must have a valid application rate. Please ensure it does and try again."
-                    )
-                    error_dialog.exec_()
-                    return False
+                    err_message = f"It looks like there is no max app rate specified for Rate 1 for {indx} in the Ag Practices Table. Rate 1 must have a valid application rate. Please ensure it does and try again."
+                    _display_error_message(error_dialog, err_message)
 
             # if MRI is specified but max app rate is not for any rate, notify user
             if any(pd.notna(l) for l in [pre_e_mri, post_e_mri]):  # if any is not nan
                 if pd.isna(max_app_rate):
-                    error_dialog.errMsgLabel.setText(
-                        f"It looks like there is no max app rate specified for rate {i} for {indx} in the Ag Practices Table but there is an MRI specified for this rate. Please ensure a max rate is specified if an MRI is specified and try again."
-                    )
-                    error_dialog.exec_()
-                    return False
+                    err_message = f"It looks like there is no max app rate specified for rate {i} for {indx} in the Ag Practices Table but there is an MRI specified for this rate. Please ensure a max rate is specified if an MRI is specified and try again."
+                    _display_error_message(error_dialog, err_message)
 
     # check the rate instructions format
     rate_instructions: list = list(
@@ -175,11 +168,8 @@ def _validate_apt(config: dict[str, Any], error_dialog: QDialog):
             results.append(False)
 
     if not all(result is True for result in results):
-        error_dialog.errMsgLabel.setText(
-            "It looks like there something wrong with the rate specific instructions in the Ag Practices Table. Please check that the formatting is valid as listed in the reader's guide and try again."
-        )
-        error_dialog.exec_()
-        return False
+        err_message = "It looks like there something wrong with the rate specific instructions in the Ag Practices Table. Please check that the formatting is valid as listed in the reader's guide and try again."
+        _display_error_message(error_dialog, err_message)
 
     return True
 
@@ -198,8 +188,7 @@ def _validate_drt(config: dict[str, Any], error_dialog: QDialog):
     try:
         drift_reduction_table.set_index(keys="Profile", inplace=True)
     except KeyError:
-        error_dialog.errMsgLabel.setText("Please ensure the first column of the DRT is 'Profile' and try again.")
-        error_dialog.exec_()
-        return False
+        err_message = "Please ensure the first column of the DRT is 'Profile' and try again."
+        _display_error_message(error_dialog, err_message)
 
     return True
